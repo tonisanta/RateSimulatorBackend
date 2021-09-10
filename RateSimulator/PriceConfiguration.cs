7235e447efe3806aa8fa52c1e3eeda0adc4ed65a
Antoni Santandreu Sureda <santandreutoni@gmail.com>
2021-09-10 21:44:04 +0200

    update default price

--- RateSimulator/PriceConfiguration.cs
+++ RateSimulator/PriceConfiguration.cs
@@ -2,10 +2,10 @@
 {
     public class PriceConfiguration
     {
-        public double PricePerKwHPunta { get; set; } = 0.223059;
-        public double PricePerKwHLlano { get; set; } = 0.125877;
-        public double PricePerKwHValle { get; set; } = 0.086632;
+        public double PricePerKwHPunta { get; set; } = 0.262682;
+        public double PricePerKwHLlano { get; set; } = 0.164195;
+        public double PricePerKwHValle { get; set; } = 0.122177;
 
-        public double PricePerKwH { get; set; } = 0.201117;
+        public double PricePerKwH { get; set; } = 0.173941;
     }
 }
